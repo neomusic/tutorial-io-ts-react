f77e566a1e537eb2fa4feac0324a55f3ea7abfbe
neomusic <giuseppe.aremare@banksealser.com>
2019-12-12 11:56:18 +0100

    feat(api): make query for request

--- src/queries/index.ts
+++ src/queries/index.ts
@@ -1,1 +1,14 @@
-export * from './queries';
+import { queryStrict, refetch } from 'avenger';
+import { getCurrentView } from 'avenger/lib/browser';
+import { search as apiSearch } from '../API';
+import { locationToView } from '../model';
+
+export const currentView = getCurrentView(locationToView);
+
+export const search = queryStrict(
+  // `queryStrict` will call this API when requested (declared in a component),
+  // and use a "strict" comparison to compare input and retrieve cached results.
+  // See https://github.com/buildo/avenger/blob/v5/README.md#queries
+  apiSearch,
+  refetch
+);
